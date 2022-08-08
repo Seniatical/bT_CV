a4402483c24797c555e6fba5d17b8c592376d8f3
Seniatical <isaja7710@gmail.com>
2022-08-08 14:58:27 +0100

    Remove random git mess

--- src/web.py
+++ src/web.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Simple http bs
 from wsgiref.simple_server import make_server
 import threading
@@ -25,31 +24,3 @@ def start():
     thread = threading.Thread(target=main, name="HTTP_SERV")
     thread.setDaemon(True)
     thread.start()
-=======
-# Simple http bs
-from wsgiref.simple_server import make_server
-import threading
-
-
-def hello_world_app(_, start_response):
-    status = '200 OK'  # HTTP Status
-    headers = [('Content-type', 'text/plain; charset=utf-8')]  # HTTP Headers
-    start_response(status, headers)
-
-    # The returned object is going to be printed
-    return [b"Hello World"]
-
-
-def main():
-    with make_server('127.0.0.1', 8000, hello_world_app) as httpd:
-        print("Serving on port 8000...")
-
-        # Serve until process is killed
-        httpd.serve_forever()
-
-
-def start():
-    thread = threading.Thread(target=main, name="HTTP_SERV")
-    thread.setDaemon(True)
-    thread.start()
->>>>>>> e58387fad4207488155264be1431bc4a52a1d868
