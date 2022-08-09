c4a2fcd4bdf037d84c4eccca7f5449cb85f72208
Seniatical <isaja7710@gmail.com>
2022-08-09 14:03:17 +0100

    Load in models

--- src/main.py
+++ src/main.py
@@ -7,6 +7,10 @@ from core.bot import bT_CV
 from core.loggers import setupLoggers
 from web import start
 
+# Any load funcs
+from commands.compare import load_model
+load_model()
+
 inst = bT_CV()
 setupLoggers()
 start()
